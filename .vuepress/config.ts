1a7258d82b193027e8f4043a714608eefa13f4e1
“zyx” <406177615@qq.com>
2023-06-02 19:28:09 +0800

    基础的信息增加

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,13 +3,13 @@ import type { DefaultThemeOptions } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 
 export default defineUserConfig({
-  title: "vuepress-theme-reco",
+  title: "zyx",
   description: "Just playing around",
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
-    logo: "/logo.png",
-    author: "reco_luan",
-    authorAvatar: "/head.png",
+    logo: "/kgdw.jpg",
+    author: "zyx",
+    authorAvatar: "/kgdw.jpg",
     docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
     docsBranch: "main",
     docsDir: "example",
@@ -28,71 +28,71 @@ export default defineUserConfig({
       ],
     },
     navbar: [
-      { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/reco/1/" },
-      { text: "Tags", link: "/tags/tag1/1/" },
+      { text: "首页", link: "/" },
+      { text: "生涯", link: "/categories/reco/1/" },
+      { text: "技能", link: "/tags/tag1/1/" },
       {
-        text: "Docs",
+        text: "其他",
         children: [
           { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
           { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
         ],
       },
     ],
-    bulletin: {
-      body: [
-        {
-          type: "text",
-          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "QQ 群",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "GitHub",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "buttongroup",
-          children: [
-            {
-              text: "打赏",
-              link: "/docs/others/donate.html",
-            },
-          ],
-        },
-      ],
-    },
+    // bulletin: {
+    //   body: [
+    //     {
+    //       type: "text",
+    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "QQ 群",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li>QQ群1：1037296104</li>
+    //         <li>QQ群2：1061561395</li>
+    //         <li>QQ群3：962687802</li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "GitHub",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "buttongroup",
+    //       children: [
+    //         {
+    //           text: "打赏",
+    //           link: "/docs/others/donate.html",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
     // commentConfig: {
     //   type: 'valie',
     //   // options 与 1.x 的 valineConfig 配置一致
--- README.md
+++ README.md
@@ -7,9 +7,9 @@ modules:
   - Footer
 bannerBrand:
   bgImage: '/bg.svg'
-  title: vuepress-reco
-  description: 一款简洁的 vuepress 博客 & 文档 主题。
-  tagline: vuepress-theme-reco 2.0 继续坚持简洁的风格，所有功能开箱即用，首页模块化组装，使用 tailwindcss 书写样式，将 Vite 作为默认编译器。你只需要负责内容创作，其他请交给我。
+  title: 欢迎来到我的世界
+  description: 一个在前端领域深耕一年的彩笔。
+  tagline: 我是一名Web前端工作者，面向于Vue和小程序方面，乐于接收新的技术，喜欢记录。我个人对于网站开发我有着浓厚的兴趣 , 喜欢用学习到的知识去设计与编写一些独特的网站。工作中会和后端 产品 ui进行高效沟通，及时完成工作需求，能够承受工作压力。工作之余经常总结新技术、新知识，并且记录在个人播客中。
   buttons:
     - { text: Guide, link: '/docs/guide/introduce' }
     - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
@@ -17,33 +17,25 @@ bannerBrand:
     - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
 blog:
   socialLinks:
-    - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+    # - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
 isShowTitleInHome: true
 actionText: About
 actionLink: /views/other/about
 ---
 
-## 快速开始
+## 个人技术栈
 
-**npx**
+1. 掌握 HTML5 和 CSS 新特性进行结构样式优化。
+2. 掌握 Vue、Vue CLI、Vuex、Vue-Router、VueUse，能够运用 Vue 全家桶进行组件化开发。
+3. 掌握 Vu3 的新特性新语法，可以使用 Vue3 新语法新特性进行各项组件开发。
+4. 熟悉 ES6、ES7 新特性，熟悉使用 JavaScript 对 DOM 的操作，配合 Vue 框架进行高效率开发。
+5. 熟悉 Less、Sass 预编译语言，提升代码的灵活性和开发效率。
+6. 熟悉 Element-UI、Vant、BootStrap 框架等组件进行页面布局和响应式开发。
+7. 熟悉 Ajax 技术进行前后端数据交互，通过 axios 高效 http 库实现动态数据渲染 。
+8. 熟悉 Git 代码版本管理工具，使用 npm 管理项目所有依赖包。
+9. 了解 node.js，express、webpack、Apache ECharts、ESLint 及其他常用开发插件的使用。
+10. 了解小程序的语法，可以通过 uni-app 进行项目开发。
+11. 熟练使用websocket及基于它所封装的socket io和sockjs来进行双向通信。
+12. 熟练使用typescript进行开发。
+13. 熟练使用nuxt进行服务端渲染.
 
-```bash
-# 初始化，并选择 2.x
-npx @vuepress-reco/theme-cli init
-```
-
-**npm**
-
-```bash
-# 初始化，并选择 2.x
-npm install @vuepress-reco/theme-cli@1.0.7 -g
-theme-cli init
-```
-
-**yarn**
-
-```bash
-# 初始化，并选择 2.x
-yarn global add @vuepress-reco/theme-cli@1.0.7
-theme-cli init
-```
